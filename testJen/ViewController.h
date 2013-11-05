fc0e8242189c4b7c7c64a5e4a0ff7b592e788571
Andrew <Andrew@iMac-Andrew.local>
2013-11-05 17:27:29 +0400

    Initial Commit

---
+++ testJen/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testJen
+//
+//  Created by Andrew on 05.11.13.
+//  Copyright (c) 2013 Andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ testJenTests/testJenTests.m
@@ -0,0 +1,34 @@
+//
+//  testJenTests.m
+//  testJenTests
+//
+//  Created by Andrew on 05.11.13.
+//  Copyright (c) 2013 Andrew. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testJenTests : XCTestCase
+
+@end
+
+@implementation testJenTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
